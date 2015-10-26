104b5a9b30730a7e47a3f2f52d03fa0b493471e6
K. Meelu <kmeelu.52@gmail.com>
2015-10-26 22:36:27 +0000

    Add Kyle's package version changes

--- models/events.js
+++ models/events.js
@@ -33,7 +33,7 @@ var allEvents = [
   },
   {
     id: 4,
-    title: 'Cookin' lessons for the busy business student',
+    title: 'Cooking lessons for the busy business student',
     date:   new Date(2014, 8, 2, 19, 0, 0),
     image: 'http://i.imgur.com/02KT9.gif',
     location: 'Yale Farm',
