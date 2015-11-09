dc86f3a4617ac32d896d5ebad87af033ca1a98ac
K. Meelu <kmeelu.52@gmail.com>
2015-11-09 04:20:47 +0000

    form-validation-for-RSVP

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ configure(app);
 app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
-app.get('/events/:id', eventControllers.eventDetail);
-app.post('/events/:id', eventControllers.rsvp);
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
+app.get('/events/:id', eventControllers.eventDetail);
+app.post('/events/:id', eventControllers.rsvp);
+app.get('/events/:id', eventControllers.eventDetail);
+
 
 module.exports = app;
